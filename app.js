be6cc42d250d36a7fb5fff4320cbee7097e49ba4
Amit Kumar <amitsingh14506@gmail.com>
2026-06-08 12:52:54 +0530

    fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('app is running');
+  res.send('app is running successfully');
 });
 
 app.listen(3000, () => {
